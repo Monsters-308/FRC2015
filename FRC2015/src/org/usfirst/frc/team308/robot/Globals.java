94d208444d6aa9d114d2c3c21b2ecc3039f5d80e
Max Hamilton <mhamilton@monsters308.org>
2015-01-23 18:22:19 -0500

    added testmode option (without PID)

--- FRC2015/src/org/usfirst/frc/team308/robot/Globals.java
+++ FRC2015/src/org/usfirst/frc/team308/robot/Globals.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team308.robot;
 
 public class Globals {
+	
+	public static boolean testMode = false;
 
 	public static double gyroPIDOutput = 0.0;
 
@@ -10,13 +12,13 @@ public class Globals {
 	public static double gyroD = 0.00085;
 
 	// motor PID constants
-	public static double talonP = 0.012;
-	public static double talonI = 0.00;
-	public static double talonD = 0.002;
+	public static double talonP = 0.1;//0.002;
+	public static double talonI = 0;//0.012;
+	public static double talonD = 0;
 
 	// maximum change in motor voltage/second (limits acceleration)
 	public static double talonRampRate = 4.0;
 
-	// maximum speed of slowest wheel (encoder ticks/10 milliseconds)
-	public static double talonMaxSpeed = 1.0;
+	// maximum speed of slowest wheel (encoder ticks/100 milliseconds)
+	public static double talonMaxSpeed = 1300.0;
 }
