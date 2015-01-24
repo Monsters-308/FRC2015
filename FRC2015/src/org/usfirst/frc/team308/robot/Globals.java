da9d42c5d2cbef61dfb61c05fcbb4c595c1d719b
Max Hamilton <mhamilton@monsters308.org>
2015-01-24 14:48:40 -0500

    more tuning

--- FRC2015/src/org/usfirst/frc/team308/robot/Globals.java
+++ FRC2015/src/org/usfirst/frc/team308/robot/Globals.java
@@ -7,17 +7,18 @@ public class Globals {
 	public static double gyroPIDOutput = 0.0;
 
 	// gyro PID constants
-	public static double gyroP = 0.0007;
-	public static double gyroI = 0.0;
-	public static double gyroD = 0.00085;
+	public static boolean userate = true;
+	public static double gyroP = 0.01;
+	public static double gyroI = 0.003;
+	public static double gyroD = 0;
 
 	// motor PID constants
-	public static double talonP = 0.1;//0.002;
-	public static double talonI = 0;//0.012;
+	public static double talonP = 0.1;//0.012;
+	public static double talonI = 0.002;//0.002;
 	public static double talonD = 0;
 
 	// maximum change in motor voltage/second (limits acceleration)
-	public static double talonRampRate = 4.0;
+	public static double talonRampRate = 36.0;
 
 	// maximum speed of slowest wheel (encoder ticks/100 milliseconds)
 	public static double talonMaxSpeed = 1300.0;
